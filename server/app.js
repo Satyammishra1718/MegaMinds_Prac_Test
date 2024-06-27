43c4ed72eb4a3e2df6186ad47d326d7fcd690b55
Sat1718 <satyam@pxal.io>
2024-06-28 01:41:19 +0530

    First Commit

--- server/app.js
+++ server/app.js
@@ -5,7 +5,13 @@ import routes from './routes/index.js';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors(
+  {
+    origin:["https://mega-minds-prac-test.vercel.app"],
+    methods:["GET", "POST"],
+    credentials:true,
+  }
+));
 app.use(express.json());
 
 app.use("/api",routes);
